e1d28f832ac4d2c9cc95d7289e597eb0eae5b497
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2021-11-23 10:31:12 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  env: {
+    API_KEY: "blt2cf669e5016d5e07",
+    DELIVERY_TOKEN: "cs24e41f5506986e69d2605fa5",
+    ENVIRONMENT_NAME: "test",
+  },
+};
