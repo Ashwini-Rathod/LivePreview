219e65aab0c5911960dd4ed0f02264708eabf032
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2021-11-24 12:41:56 +0530

    test11

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ function Home(props) {
     console.log("DATA", data)
     if(data.hasOwnProperty('hash')){
       console.log("HERERERERER", data['hash'], data['content_type_uid'])
-      let res = await fetch(`${testURL}live_preview=${data['hash']}&content_type_uid=${data['content_type_uid']}`, {
+      let res = await fetch(`${url}&live_preview=${data['hash']}&content_type_uid=${data['content_type_uid']}`, {
         headers: {
           api_key: process.env.API_KEY,
           access_token: process.env.DELIVERY_TOKEN
