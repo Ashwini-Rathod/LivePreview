a065e77173b4b2325c3c2a816d0ae99a747f47f4
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2021-12-24 16:17:10 +0530

    Sample next.congif file added

---
+++ next.config.sample.js
@@ -0,0 +1,8 @@
+// filename should be next.config.js
+module.exports = {
+    env: {
+      API_KEY: "api_key",
+      DELIVERY_TOKEN: "delivery_token",
+      ENVIRONMENT_NAME: "environment",
+    },
+  };
